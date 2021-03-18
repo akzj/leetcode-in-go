e32d28fa80665c68b661e267a6af438284cbeecf
akzi <fu.niukey@gmail.com>
2021-03-18 10:01:00 +0800

    update

--- construct_binary_tree_from_preorder_and_inorder_traversal_105/solution_test.go
+++ construct_binary_tree_from_preorder_and_inorder_traversal_105/solution_test.go
@@ -1,9 +1,11 @@
 package construct_binary_tree_from_preorder_and_inorder_traversal_105
 
 import (
+	"github.com/akzj/leetcode-in-go/structures"
+	"github.com/stretchr/testify"
 	"testing"
 )
 
 func TestBuildTree(t *testing.T) {
-
+	assert.True(t,true)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/akzj/leetcode-in-go
 
-go 1.15
+go 1.16
+
+require github.com/stretchr/testify v1.6.1
